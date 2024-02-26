83699b156f97306011647d81a65ef4083d7d4497
goblincore <withallmyhrt@gmail.com>
2024-02-26 18:20:18 -0500

    temporarily disable endpoint

--- src/endpoints/getResource.ts
+++ src/endpoints/getResource.ts
@@ -6,15 +6,15 @@ export const getResource: PayloadHandler = async (req, res) => {
     console.log('///req query', req?.query?.resource);
     try {
         if(!req?.query?.resource) throw new Error('No resource id provided');
-        const resource = await payload.findByID({
-            id: req?.query?.resource,
-            collection: 'resource',
-            depth: 3,
-        });
+        // const resource = await payload.findByID({
+        //     id: req?.query?.resource,
+        //     collection: 'resource',
+        //     depth: 3,
+        // });
 
-        console.log('///resource', resource);
+        // console.log('///resource', resource);
 
-        res.status(200).json(resource);;
+        res.status(200).json('test');;
     } catch (err) {
         console.error(err);
         res.status(500).json({ err });
